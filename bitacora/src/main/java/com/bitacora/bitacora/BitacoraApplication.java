c5b6ba5b9d22f9b54841ebfa5997c4ced9cb1911
Ulises Caceres <uliseo_95@hotmail.com>
2024-02-25 12:25:14 -0600

    Primer Commit

---
+++ bitacora/src/main/java/com/bitacora/bitacora/BitacoraApplication.java
@@ -0,0 +1,13 @@
+package com.bitacora.bitacora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitacoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitacoraApplication.class, args);
+	}
+
+}
---
+++ bitacora/src/test/java/com/bitacora/bitacora/BitacoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitacora.bitacora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitacoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
